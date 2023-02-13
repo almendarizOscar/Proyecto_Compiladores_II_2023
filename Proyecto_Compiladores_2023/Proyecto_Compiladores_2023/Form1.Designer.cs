50ec0f7421f6562095b4a37daff652634a6fcd4c
Oscar Almendariz Rodriguez <A254933@alumnos.uaslp.mx>
2023-02-13 11:07:32 -0600

    Se agregaron solo los metodos que se van a utilizar. Cada método ya está asignado.

--- Proyecto_Compiladores_2023/Proyecto_Compiladores_2023/Form1.Designer.cs
+++ Proyecto_Compiladores_2023/Proyecto_Compiladores_2023/Form1.Designer.cs
@@ -28,13 +28,129 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.BotonConvertirPosfija = new System.Windows.Forms.Button();
+			this.txtPostfija = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.ExpresionRegular = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label5
+			// 
+			this.label5.BackColor = System.Drawing.Color.LimeGreen;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.ForeColor = System.Drawing.Color.White;
+			this.label5.Location = new System.Drawing.Point(134, 17);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(578, 55);
+			this.label5.TabIndex = 6;
+			this.label5.Text = "Oscar Almendariz Rodríguez\r\nJosé Feliciano Bautista Martinez";
+			this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.BackColor = System.Drawing.Color.LimeGreen;
+			this.label4.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label4.Location = new System.Drawing.Point(202, 8);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(428, 23);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "Fundamentos de compiladores - Equipo Hopper";
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.LimeGreen;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(-216, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(1177, 72);
+			this.label1.TabIndex = 4;
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// BotonConvertirPosfija
+			// 
+			this.BotonConvertirPosfija.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.BotonConvertirPosfija.Location = new System.Drawing.Point(131, 130);
+			this.BotonConvertirPosfija.Name = "BotonConvertirPosfija";
+			this.BotonConvertirPosfija.Size = new System.Drawing.Size(140, 21);
+			this.BotonConvertirPosfija.TabIndex = 11;
+			this.BotonConvertirPosfija.Text = "Convertir a postfija";
+			this.BotonConvertirPosfija.UseVisualStyleBackColor = true;
+			this.BotonConvertirPosfija.Click += new System.EventHandler(this.btnRegularPostija_Click);
+			// 
+			// txtPostfija
+			// 
+			this.txtPostfija.Font = new System.Drawing.Font("Microsoft Tai Le", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtPostfija.Location = new System.Drawing.Point(15, 155);
+			this.txtPostfija.Name = "txtPostfija";
+			this.txtPostfija.Size = new System.Drawing.Size(256, 19);
+			this.txtPostfija.TabIndex = 10;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(12, 141);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(42, 13);
+			this.label3.TabIndex = 9;
+			this.label3.Text = "Postfija";
+			// 
+			// ExpresionRegular
+			// 
+			this.ExpresionRegular.Font = new System.Drawing.Font("Microsoft Tai Le", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ExpresionRegular.Location = new System.Drawing.Point(15, 106);
+			this.ExpresionRegular.Name = "ExpresionRegular";
+			this.ExpresionRegular.Size = new System.Drawing.Size(256, 19);
+			this.ExpresionRegular.TabIndex = 8;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(12, 91);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(91, 13);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "Expresión Regular";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(800, 415);
+			this.Controls.Add(this.BotonConvertirPosfija);
+			this.Controls.Add(this.txtPostfija);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.ExpresionRegular);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button BotonConvertirPosfija;
+		private System.Windows.Forms.TextBox txtPostfija;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox ExpresionRegular;
+		private System.Windows.Forms.Label label2;
 	}
 }
 
--- Proyecto_Compiladores_2023/Proyecto_Compiladores_2023/Form1.cs
+++ Proyecto_Compiladores_2023/Proyecto_Compiladores_2023/Form1.cs
@@ -16,5 +16,37 @@ namespace Proyecto_Compiladores_2023
 		{
 			InitializeComponent();
 		}
+		//[Oscar Almendariz R]
+		//Boton que contiene la secuencia de pasos que se deben aplicar para obtener la expresión posfija de
+		//la expresión regular
+		private void btnRegularPostija_Click(object sender, EventArgs e)
+		{			
+			string expresionRegular = this.ExpresionRegular.Text; //Obtenemos la expresión regular
+			expresionRegular = convertir_rango(expresionRegular);
+			expresionRegular = agregar_amperson(expresionRegular);
+			ExpresionRegular.Text = expresionRegular; //Muestra la expresion regular con & y los rangos extendidos			
+			txtPostfija.Text = postfija(expresionRegular); //Convierte la expresión regular en postfija
+		}
+		//[Ivan & Oscar Almendariz ]
+		private string postfija(string expresion_regular)
+		{
+			return "";
+		}
+		//[Oscar Almendariz]
+		private string convertir_rango(string expresion_regular)
+		{
+			return "";
+		}
+		//[Ivan]
+		private string agregar_amperson(string expresion_regular)
+		{
+			return "";
+		}
+
+		//[Oscar Almendariz]
+		private void limpiar_interfaz()
+		{
+			BotonConvertirPosfija.Enabled = false;
+		}
 	}
 }
